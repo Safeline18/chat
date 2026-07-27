c513a21309fe989f9a91dd1e80d2e8d73a5c616a
Mohisvst29 <mohmad.shiple@gmail.com>
2026-07-27 23:41:31 +0300

    Export Express app directly in api/index.js to prevent Vercel invocation crash

--- api/index.js
+++ api/index.js
@@ -1,11 +1,3 @@
 const app = require('../server');
-const { initDatabase } = require('../src/database');
 
-module.exports = async (req, res) => {
-  try {
-    await initDatabase();
-  } catch (e) {
-    console.warn('⚠️ Serverless DB init warning:', e.message);
-  }
-  return app(req, res);
-};
+module.exports = app;
